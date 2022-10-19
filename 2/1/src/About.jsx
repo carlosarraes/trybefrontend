51783bd09c8f87ae9df26b1b78a9a65f6d08c3ee
carraes <carraeshb@gmail.com>
2022-10-19 10:13:08 -0300

    chore: frontend - ex 2.1

---
+++ 2/1/src/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>Carlos Arraes</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore, aut!</p>
+      <h2>Minhas Habilidades</h2>
+      <ul>
+        <li>Html</li>
+        <li>css</li>
+        <li>javascript</li>
+      </ul>
+    </div>
+  )
+}
+
+export default About
