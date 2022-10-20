2bcdaa5d512a62d85aa89f3c12d6965b7382af0b
carraes <carraeshb@gmail.com>
2022-10-20 08:34:44 -0300

    build: exercicios 2.1

--- 2/1/src/App.jsx
+++ 2/1/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import About from './About';
+import Image from './Image';
+import catPic from './images/cat-2083492_1280.jpg'
 
 class App extends React.Component {
   render() {
     return (
       <>
         <About />
+        <Image source={catPic} alternativeText='Cute cat staring' />
       </>
     );
   }
