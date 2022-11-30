97251a60908debe205a9185e2bb5dd1f430dbc1e
carraes <carraeshb@gmail.com>
2022-11-29 23:08:36 -0300

    build: ex1 done

--- 7/1/main.js
+++ 7/1/main.js
@@ -4,3 +4,51 @@ const INITIAL_STATE = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
+const nextBtn = document.querySelector('#next');
+const prevBtn = document.querySelector('#previous');
+const randomBtn = document.querySelector('#random');
+const showValue = document.querySelector('#value');
+
+const getRandomColor = () => Math.floor(Math.random() * 0xffffff).toString(16);
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'NEXT_COLOR':
+      return {
+        ...state,
+        index: state.index === state.colors.length - 1 ? state.colors.length - 1 : state.index + 1,
+      };
+    case 'PREVIOUS_COLOR':
+      return {
+        ...state,
+        index: state.index === 0 ? 0 : state.index - 1,
+      };
+    case 'RANDOM_COLOR':
+      return {
+        ...state,
+        colors: [...state.colors, getRandomColor()],
+        index: state.colors.length,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  const { colors, index } = store.getState();
+  showValue.textContent = colors[index];
+});
+
+nextBtn.addEventListener('click', () => {
+  store.dispatch({ type: 'NEXT_COLOR' });
+});
+
+prevBtn.addEventListener('click', () => {
+  store.dispatch({ type: 'PREVIOUS_COLOR' });
+});
+
+randomBtn.addEventListener('click', () => {
+  store.dispatch({ type: 'RANDOM_COLOR' });
+});
